678c49faf9d697be8c5469f1f2dead1a8fc53b76
Christian P <christian@cpj.nu>
2021-02-27 01:05:35 +0100

    Small change

--- index.js
+++ index.js
@@ -15,7 +15,8 @@ const filter = (reaction, user) =>{
     return reaction.emoji.name.startsWith("bonk");
 }
 let hornyLimit = 5;
-let collectorOptions = {time: 60*60*1000, max: 32};
+let collectorOptions = {time: 60*60*1000, max: 1};
+let scoreboardCollectorOptions = {time: 60*60*1000, max: 1};
 let collectors = [];
 let config = undefined;
 let activeGuild = undefined;
@@ -165,7 +166,7 @@ dc.on("message", message=>{
         handleCommandMessage(message, cmd);
     } else {
 
-        message.createReactionCollector(()=>true, collectorOptions).on("collect", onReactScoreboard);
+        message.createReactionCollector(()=>true, scoreboardCollectorOptions).on("collect", onReactScoreboard);
         config.roles.forEach(role=>{
             let filter = (reaction, user) =>{
                 return reaction.emoji.name == (role.emoji);
