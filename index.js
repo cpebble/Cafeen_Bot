9a3e31fc71babf7f876ab762ff0a0e0a6b10cbeb
Christian P <christian@cpj.nu>
2021-04-07 02:08:20 +0200

    Cleanup

--- index.js
+++ index.js
@@ -11,32 +11,18 @@ const rl = readline.createInterface({
 });
 const utils = require("./utils.js");
 
+// Config init
 let config = undefined;
 
-// Exit cleanly
-function exitHandler(options, exitCode){
-    if (options.cleanup){
-        // Destroy modules
-        fs.writeFileSync("config.json", JSON.stringify(config));
-        Scoreboard.destroy(dc, config);
-        Quotes.destroy(dc, config);
-        Jail.destroy(dc, config);
-    } 
-    if (exitCode || exitCode === 0) console.log(exitCode);
-    if (options.exit){
-        console.log("Exit cleanly");
-        process.exit();
-    } 
-}
 
-function handleCommandMessage (message, cmd){
+function handleCommandMessage(message, cmd) {
     let response = handleCommand(cmd);
     message.channel.send(response);
 }
 
 // Listeners
-dc.on("message", message=>{
-    if (message.content.startsWith(config.command_char)){
+dc.on("message", message => {
+    if (message.content.startsWith(config.command_char)) {
         let cmd = message.content.substring(1)
         console.log(`Got command: ${cmd}`);
         handleCommandMessage(message, cmd);
@@ -45,11 +31,10 @@ dc.on("message", message=>{
     }
 });
 
-
 // This handles input from either cli or bot dm
-function handleCommand(cmd){
+function handleCommand(cmd) {
     let cmdArg = cmd.split(" ")
-    switch (cmdArg[0]){
+    switch (cmdArg[0]) {
         // case 'reload_conf':
         //     loadConfigFile();
         //     return "Reload conf started";
@@ -93,48 +78,66 @@ function handleCommand(cmd){
 // Start the bot server
 console.log("Logging in with" + secrets["discord-api-token"]);
 dc.login(secrets["discord-api-token"]);
+// TODO: Fix
 rl.prompt();
-rl.on('line', (line)=>{
+rl.on('line', (line) => {
     let res = handleCommand(line.trim());
     console.log(res);
     rl.prompt();
-})
+});
+// Exit cleanly
+function exitHandler(options, exitCode) {
+    if (options.cleanup) {
+        // Destroy modules
+        fs.writeFileSync("config.json", JSON.stringify(config));
+        Scoreboard.destroy(dc, config);
+        Quotes.destroy(dc, config);
+        Jail.destroy(dc, config);
+    }
+    if (exitCode || exitCode === 0) console.log(exitCode);
+    if (options.exit) {
+        console.log("Exit cleanly");
+        process.exit();
+    }
+}
 // Exit binds
-process.on('exit', exitHandler.bind(null,{cleanup:true, exit: true}));
-
+process.on('exit', exitHandler.bind(null, { cleanup: true, exit: true }));
 //catches ctrl+c event
-process.on('SIGINT', exitHandler.bind(null, {cleanup:true,exit:true}));
-
+process.on('SIGINT', exitHandler.bind(null, { cleanup: true, exit: true }));
 // catches "kill pid" (for example: nodemon restart)
-process.on('SIGUSR1', exitHandler.bind(null, {exit:true}));
-process.on('SIGUSR2', exitHandler.bind(null, {exit:true}));
-
+process.on('SIGUSR1', exitHandler.bind(null, { exit: true }));
+process.on('SIGUSR2', exitHandler.bind(null, { exit: true }));
 //catches uncaught exceptions
-process.on('uncaughtException', exitHandler.bind(null, {exit:true}));
+process.on('uncaughtException', exitHandler.bind(null, { exit: true }));
 
-utils.loadJsonFile("config").then(data=>config = data)
 
-dc.once("ready", ()=>{
+// Startup binds
+dc.once("ready", () => {
     console.log("Discord ready");
     dc.user.setActivity('Your conversations', { type: 'LISTENING' });
-    init().then(()=>{
+    init().then(() => {
         console.log("Main init() reported done");
     })
 });
-async function init(){
-// Load the config file
+async function init() {
+    // Load the config file
     await RegisterModules();
 }
 
+// Load conf before anything else
+utils.loadJsonFile("config").then(data => config = data)
+
 // Module loading
 const Scoreboard = require("./scoreboard");
-const Quotes     = require("./quotes");
-const Jail       = require("./jail");
+const Quotes = require("./quotes");
+const Jail = require("./jail");
 
-async function RegisterModules(){
+// Async load func
+async function RegisterModules() {
     // Gather promises
     let sP = Scoreboard.init(dc, config);
-    let qP = Quotes.init(dc,config);
+    let qP = Quotes.init(dc, config);
     let jP = Jail.init(dc, config);
+    // Load async
     await Promise.all([sP, qP, jP]);
 }
