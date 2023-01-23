71725fefda7f9c8b6ff690e4c221b19342fde3ac
Christian Påbøl <cpcp800@auroragames.dk>
2023-01-23 13:37:52 +0100

    Chagne to sequel-typescript

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,7 +1,5 @@
 import config from "../config.json";
-import * as SQ from "sequelize";
-import { DataType } from "sequelize-typescript";
-import { InferCreationAttributes, Model } from "sequelize";
+import * as SQ from "sequelize-typescript";
 
 export class DBM {
     // Singleton
@@ -35,69 +33,46 @@ export class DBM {
         await this.sq.authenticate()
     }
 
-    public async registerModel(model: SQ.ModelStatic<any>, initObj){
-        let sq = this.sq;
-        model.init(initObj, {sequelize: sq});
-        await this.sq.sync({alter: true});
-        return true;
+    public async addModel(model: SQ.ModelCtor<SQ.Model<any, any>>){
+        this.sq.addModels([model]);
+    }
+    /// Probably shouldn't be used
+    public async syncDB(alter: boolean = false){
+        return this.sq.sync({alter: alter})
     }
 }
 
-// Types for declaring and interfacing
-type MDecl = {
-    table: string
-    fields: {
-        [col: string]: SQ.DataType
-    }
-}
-type MProps<T extends MDecl> = {
-    [Prop in keyof T['fields']]
-}
 
-// Testing can be deleted
-const Pa = {
-    "table": "Person",
-    "fields": {
-        "name": SQ.STRING,
-        "bday": SQ.DATE
-    }
-};
-function getObj<T extends MDecl>(obj: T) {
-    return obj.fields as MProps<T>;
-}
-let b: MProps<typeof Pa> = getObj(Pa);
+// _____ _____ ____ _____ 
+//|_   _| ____/ ___|_   _|
+//  | | |  _| \___ \ | |  
+//  | | | |___ ___) || |  
+//  |_| |_____|____/ |_|  
 
-// Wrapper class
-// Might not be relevant idc
-// class DBModel<MD extends MDecl> {
-//     private model;
-
-//     public tableName;
-
-//     constructor(decl: MD) {
-//         this.tableName = decl.table;
-//     }
-// }
-
-class DBObj<A extends SQ.Model<any, any>> 
-        extends SQ.Model<SQ.InferAttributes<A>,SQ.InferCreationAttributes<A>> {
-    static initObj: SQ.ModelAttributes;
-}
-class Quote extends DBObj<Quote> {
-    declare author: string;
-    declare text: string;
-    declare snowflake: string;
-    static initObj = {
-        "author": SQ.DataTypes.STRING,
-        "text": SQ.DataTypes.TEXT,
-        "snowflake": SQ.DataTypes.STRING
-    }
+@SQ.Table
+class Quote extends SQ.Model<Quote> {
+    @SQ.AllowNull(false)
+    @SQ.Column
+    declare Author: string;
+    @SQ.Column(SQ.DataType.TEXT)
+    declare Text: string;
+    @SQ.Column
+    declare Snowflake?: string;
 }
 
-// Testing code
 (async ()=>{
     let t = await DBM.getInstance();
-    console.log("SQL Is Up and Running");
-    Quote
-    //let m = t.registerModel(Quote);
+    await t.addModel(Quote);
+    console.log("Added Quote model");
+    await Quote.sync();
+    console.log("Synced");
+
+    let p = new Quote({Author: "Pebble", Text: "Fuack"});
+    await p.save();
+    console.log("Saved");
+
+    let p_ = await Quote.findAll();
+    console.log(p_[0].Author)
+    // console.log(`${p_.id}: ${p_.Author}`);
+    
 })()
